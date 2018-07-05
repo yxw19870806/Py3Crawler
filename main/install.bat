cb6eca31e5080d9383c905bc4b0e1ac4b7aa85b5
hikaru <hikaru870806@hotmail.com>
2018-07-05 15:25:55 +0800

    install bat，pyhook和PyCrypto需要重新寻找对应版本，暂时注释掉

--- main/install.bat
+++ main/install.bat
@@ -1,24 +1,24 @@
 @echo off
 echo update pip
-C:\Python27\python.exe -m pip install --upgrade pip
+python.exe -m pip install --upgrade pip
 
 echo install Pillow
-C:\python27\Scripts\pip.exe install Pillow
-
-echo install PyHook
-C:\python27\Scripts\pip.exe install pyHook
-
-echo install PyWin32
-C:\python27\Scripts\pip.exe install pywin32
-C:\Python27\python.exe C:\python27\Scripts\pywin32_postinstall.py -install
-
-echo install PyCrypto
-C:\python27\Scripts\pip.exe install PyCrypto
+pip.exe install Pillow
 
 echo install Urllib3
-C:\python27\Scripts\pip.exe install urllib3
+pip.exe install urllib3
 
 echo install PyQuery
-C:\python27\Scripts\pip.exe install pyquery
+pip.exe install pyquery
+
+::echo install PyHook
+::pip.exe install pyHook
+
+::echo install PyWin32
+::pip.exe install pywin32
+::python.exe pywin32_postinstall.py -install
+
+::echo install PyCrypto
+::pip.exe install PyCrypto
 
 pause
