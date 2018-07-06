d70712227728f98e785331a152c03fcd750ad440
hikaru <hikaru870806@hotmail.com>
2018-07-06 14:44:23 +0800

    update

--- project/meipai/extra.py
+++ project/meipai/extra.py
@@ -18,18 +18,18 @@ def get_follow_list(account_id):
         follow_pagination_url = "http://www.meipai.com/user/%s/friends" % account_id
         query_data = {"p": page_count}
         follow_pagination_response = net.http_request(follow_pagination_url, method="GET", fields=query_data)
-        if follow_pagination_response.status == net.HTTP_RETURN_CODE_SUCCEED:
-            follow_list_find = re.findall('<div class="ucard-info">([\s|\S]*?)</div>', follow_pagination_response.data)
-            for follow_info in follow_list_find:
-                follow_account_id = tool.find_sub_string(follow_info, '<a hidefocus href="/user/', '"').strip()
-                follow_account_name = tool.find_sub_string(follow_info, 'title="', '"')
-                follow_list[follow_account_id] = follow_account_name
-            if max_page_count == 1:
-                page_info = tool.find_sub_string(follow_pagination_response.data, '<div class="paging-wrap">', "</div>")
-                if page_info:
-                    page_count_find = re.findall("friends\?p=(\d*)", page_info)
-                    max_page_count = max(list(map(int, page_count_find)))
-            page_count += 1
-        else:
+        if follow_pagination_response.status != net.HTTP_RETURN_CODE_SUCCEED:
             return None
+        follow_pagination_response_content = follow_pagination_response.data.decode()
+        follow_list_find = re.findall('<div class="ucard-info">([\s|\S]*?)</div>', follow_pagination_response_content)
+        for follow_info in follow_list_find:
+            follow_account_id = tool.find_sub_string(follow_info, '<a hidefocus href="/user/', '"').strip()
+            follow_account_name = tool.find_sub_string(follow_info, 'title="', '"')
+            follow_list[follow_account_id] = follow_account_name
+        if max_page_count == 1:
+            page_info = tool.find_sub_string(follow_pagination_response_content, '<div class="paging-wrap">', "</div>")
+            if page_info:
+                page_count_find = re.findall("friends\?p=(\d*)", page_info)
+                max_page_count = max(list(map(int, page_count_find)))
+        page_count += 1
     return follow_list
