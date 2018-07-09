a569519bc31ddf03793d65273e8bbe916d7fe48b
hikaru <hikaru870806@hotmail.com>
2018-07-09 22:04:16 +0800

    rename

--- common/crypto.py
+++ common/crypto.py
@@ -18,21 +18,21 @@ class Crypto():
         加解密算法为AES，密钥位长128，CBC模式，填充标准PKCS7
         签名算法为SHA256的HMAC，密钥位长128位
     """
-    PRIVATE_KEY = "#@Py3Crawl@#"
+    SALT = "#@Py3Crawl@#"
 
     def __init__(self):
         # MAC + 固定字符串 + 计算机名，生成基本上唯一的加密私钥
-        self.PRIVATE_KEY = str(hex(uuid.getnode())) + self.PRIVATE_KEY + socket.gethostname()
+        PRIVATE_KEY = hex(uuid.getnode()) + self.SALT + socket.gethostname()
 
         # 任意字符串MD5后生成32位的key，然后base64
-        self.KEY = base64.urlsafe_b64encode(hashlib.md5(self.PRIVATE_KEY.encode("UTF-8")).hexdigest().encode("UTF-8"))
+        self.PRIVATE_KEY = base64.urlsafe_b64encode(hashlib.md5(PRIVATE_KEY.encode("UTF-8")).hexdigest().encode("UTF-8"))
 
     def encrypt(self, s):
         if not isinstance(s, bytes):
             s = str(s).encode("UTF-8")
-        f = Fernet(self.KEY)
+        f = Fernet(self.PRIVATE_KEY)
         return f.encrypt(s).decode()
 
     def decrypt(self, s):
-        f = Fernet(self.KEY)
+        f = Fernet(self.PRIVATE_KEY)
         return f.decrypt(s.encode()).decode()
