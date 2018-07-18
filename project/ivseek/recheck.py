db8124cf47f257ca8f955d0c54069cbf5e07c6a7
hikaru <hikaru870806@hotmail.com>
2018-07-18 11:06:32 +0800

    PEP8

--- project/ivseek/recheck.py
+++ project/ivseek/recheck.py
@@ -4,14 +4,14 @@
 email: hikaru870806@hotmail.com
 如有问题或建议请联系
 """
-from common import *
 import os
+from common import *
 
-DONE_SING= "~"
+DONE_SING = "~"
 
 
 def read_save_data(save_data_path):
-    result_list= []
+    result_list = []
     if not os.path.exists(save_data_path):
         return result_list
     for single_save_data in tool.read_file(save_data_path, tool.READ_FILE_TYPE_LINE):
