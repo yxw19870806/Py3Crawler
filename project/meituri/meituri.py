4d6126e15a35ddea012c31ca7383666b2019759c
hikaru <hikaru870806@hotmail.com>
2018-07-31 23:35:01 +0800

    增加host

--- project/meituri/meituri.py
+++ project/meituri/meituri.py
@@ -42,7 +42,7 @@ def get_album_page(album_id):
             album_pagination_url = "http://www.meituri.com/a/%s/" % album_id
         else:
             album_pagination_url = "http://www.meituri.com/a/%s/%s.html" % (album_id, page_count)
-        album_pagination_response = net.http_request(album_pagination_url, method="GET")
+        album_pagination_response = net.http_request(album_pagination_url, method="GET", header_list={"Host": "www.meituri.com"})
         if page_count == 1 and album_pagination_response.status in [403, 404]:
             result["is_delete"] = True
             return result
