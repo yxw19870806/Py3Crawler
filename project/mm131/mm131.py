adfc0ac991f47cfc124ea2190353ecda5993d248
hikaru <hikaru870806@hotmail.com>
2018-07-31 22:04:21 +0800

    rename

--- project/mm131/mm131.py
+++ project/mm131/mm131.py
@@ -85,7 +85,7 @@ def get_album_page(album_id):
     return result
 
 
-class Gallery(crawler.Crawler):
+class MM131(crawler.Crawler):
     def __init__(self):
         # 设置APP目录
         tool.PROJECT_APP_PATH = os.path.abspath(os.path.dirname(__file__))
@@ -176,4 +176,4 @@ class Gallery(crawler.Crawler):
 
 
 if __name__ == "__main__":
-    Gallery().main()
+    MM131().main()
