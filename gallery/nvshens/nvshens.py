ff6d6dcc827ed27bcf252b2fdfb1a7ab02f1b2f9
hikaru <hikaru870806@hotmail.com>
2018-08-08 11:43:02 +0800

    日志输出使用原始标题文本

--- gallery/nvshens/nvshens.py
+++ gallery/nvshens/nvshens.py
@@ -154,7 +154,7 @@ class Nvshens(crawler.Crawler):
                 for image_url in album_response["image_url_list"]:
                     if not self.is_running():
                         tool.process_exit(0)
-                    log.step("图集%s《%s》开始下载第%s张图片 %s" % (album_id, album_title, image_index, image_url))
+                    log.step("图集%s《%s》开始下载第%s张图片 %s" % (album_id, album_response["album_title"], image_index, image_url))
 
                     file_type = image_url.split(".")[-1]
                     file_path = os.path.join(album_path, "%03d.%s" % (image_index, file_type))
@@ -169,9 +169,9 @@ class Nvshens(crawler.Crawler):
                         if new_image_url is not None:
                             save_file_return = net.save_net_file(new_image_url, file_path, header_list=header_list)
                     if save_file_return["status"] == 1:
-                        log.step("图集%s《%s》第%s张图片下载成功" % (album_id, album_title, image_index))
+                        log.step("图集%s《%s》第%s张图片下载成功" % (album_id, album_response["album_title"], image_index))
                     else:
-                        log.error("图集%s《%s》第%s张图片 %s 下载失败，原因：%s" % (album_id, album_title, image_index, image_url, crawler.download_failre(save_file_return["code"])))
+                        log.error("图集%s《%s》第%s张图片 %s 下载失败，原因：%s" % (album_id, album_response["album_title"], image_index, image_url, crawler.download_failre(save_file_return["code"])))
                     image_index += 1
                 # 图集内图片全部下载完毕
                 temp_path = ""  # 临时目录设置清除
