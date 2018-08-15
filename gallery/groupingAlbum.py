cf289bcac58e9551ae12b87fba6ecde3562623d1
hikaru <hikaru870806@hotmail.com>
2018-08-15 14:40:51 +0800

    去除图集分类

--- gallery/groupingAlbum.py
+++ gallery/groupingAlbum.py
@@ -9,7 +9,6 @@ from common import *
 
 ALBUM_ROOT_PATH = "D:\\album"
 RESULT_FILE_PATH = "D:\\result"
-ALBUM_CATEGORY_LIST = []
 
 
 def main():
@@ -27,14 +26,8 @@ def main():
             for file_name in path.get_dir_files_name(album_path):
                 file_size += os.path.getsize(os.path.join(album_path, file_name))
                 file_count += 1
-            # 图集分类
-            album_category = ""
-            for temp_album_category in ALBUM_CATEGORY_LIST:
-                if album_name.find(temp_album_category) >= 0:
-                    album_category = album_category
-                    break
-            # 图集id  图集名字  图集分类  图片数量  图片总大小
-            tool.write_file("%s\t%s\t%s\t%s\t%s" % (str(int(album_id)), album_name, album_category, file_count, file_size), website_result_file_path)
+            # 图集id  图集名字  图片数量  图片总大小
+            tool.write_file("%s\t%s\t%s\t%s" % (str(int(album_id)), album_name, file_count, file_size), website_result_file_path)
 
 
 if __name__ == "__main__":
