088a1f7a29da1add520467927f9c23360a69b982
hikaru <hikaru870806@hotmail.com>
2018-08-16 17:05:17 +0800

    指定全名K歌歌曲下载

---
+++ project/kg/download.py
@@ -0,0 +1,57 @@
+# -*- coding:UTF-8  -*-
+"""
+指定全名K歌歌曲下载
+http://kg.qq.com/
+@author: hikaru
+email: hikaru870806@hotmail.com
+如有问题或建议请联系
+"""
+import os
+from common import *
+from project.kg import kg
+
+DOWNLOAD_FILE_PATH = os.path.join(os.path.dirname(__file__), "video")
+
+
+def main():
+    config = crawler._get_config()
+    # 设置日志路径
+    crawler.quicky_set_log_path(config)
+    # 设置代理
+    crawler.quickly_set_proxy(config)
+
+    while True:
+        audio_url = input(crawler.get_time() + " 请输入全名K歌歌曲地址：")
+        audio_id = None
+        # http://node.kg.qq.com/play?s=JLm8h1J64lEyXJ6n&g_f=personal
+        if audio_url.lower().find("//node.kg.qq.com/play") > 0:
+            query_string_list = audio_url.split("?")[-1].split("&")
+            for query_string in query_string_list:
+                if query_string.find("=") == -1:
+                    continue
+                key, value = query_string.split("=", 1)
+                if key == "s":
+                    audio_id = value
+                    break
+        if audio_id is None:
+            log.step("错误的歌曲地址，正确的地址格式如：http://node.kg.qq.com/play?s=JLm8h1J64lEyXJ6n")
+            continue
+        # 访问歌曲播放页
+        try:
+            audio_response = kg.get_audio_play_page(audio_id)
+        except crawler.CrawlerException as e:
+            log.error("解析歌曲下载地址失败，原因：%s" % e.message)
+            tool.process_exit()
+        # 开始下载
+        file_type = kg.get_file_type(audio_response["audio_url"])
+        file_path = os.path.abspath(os.path.join(DOWNLOAD_FILE_PATH, "%s - %s.%s" % (audio_id, path.filter_text(audio_response["audio_title"]), file_type)))
+        log.step("\n歌曲标题：%s\n歌曲地址：%s\n下载路径：%s" % (audio_response["audio_title"], audio_response["audio_url"], file_path))
+        save_file_return = net.save_net_file(audio_response["audio_url"], file_path, head_check=True)
+        if save_file_return["status"] == 1:
+            log.step("歌曲《%s》下载成功" % audio_response["audio_title"])
+        else:
+            log.error("歌曲《%s》下载失败，原因：%s" % (audio_response["audio_title"], crawler.download_failre(save_file_return["code"])))
+
+
+if __name__ == "__main__":
+    main()
