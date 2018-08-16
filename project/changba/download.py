0293039e5ca3b46d7417642d68f878edabe1ce39
hikaru <hikaru870806@hotmail.com>
2018-08-16 16:25:51 +0800

    update

--- project/changba/download.py
+++ project/changba/download.py
@@ -23,20 +23,19 @@ def main():
     while True:
         audio_url = input(crawler.get_time() + " 请输入唱吧歌曲地址：")
         # https://changba.com/s/LBdSlkRwmqApasSCCVp5VA
-        if audio_url.lower().find("//changba.com/s/") > 0:
-            audio_key = audio_url.split("/")[-1].split("?")[0]
-        else:
+        if audio_url.lower().find("//changba.com/s/") == -1:
             log.step("错误的歌曲地址，正确的地址格式如：https://changba.com/s/LBdSlkRwmqApasSCCVp5VA")
             continue
+        audio_id = audio_url.split("/")[-1].split("?")[0]
         # 访问歌曲播放页
         try:
-            audio_response = changba.get_audio_play_page(audio_key)
+            audio_response = changba.get_audio_play_page(audio_id)
         except crawler.CrawlerException as e:
             log.error("解析歌曲下载地址失败，原因：%s" % e.message)
             tool.process_exit()
         # 开始下载
         file_type = audio_response["audio_url"].split(".")[-1]
-        file_path = os.path.abspath(os.path.join(DOWNLOAD_FILE_PATH, "%s - %s.%s" % (audio_key, path.filter_text(audio_response["audio_title"]), file_type)))
+        file_path = os.path.abspath(os.path.join(DOWNLOAD_FILE_PATH, "%s - %s.%s" % (audio_id, path.filter_text(audio_response["audio_title"]), file_type)))
         log.step("\n歌曲标题：%s\n歌曲地址：%s\n下载路径：%s" % (audio_response["audio_title"], audio_response["audio_url"], file_path))
         save_file_return = net.save_net_file(audio_response["audio_url"], file_path, head_check=True)
         if save_file_return["status"] == 1:
