42358b48af3f7a2d647ae0256a495544c492641e
hikaru <hikaru870806@hotmail.com>
2018-08-17 17:56:19 +0800

    增加对话框，选择下载文件的保存目录

--- project/kg/download.py
+++ project/kg/download.py
@@ -1,12 +1,14 @@
 # -*- coding:UTF-8  -*-
 """
-指定全名K歌歌曲下载
+指定全民K歌歌曲下载
 https://kg.qq.com/
 @author: hikaru
 email: hikaru870806@hotmail.com
 如有问题或建议请联系
 """
 import os
+import tkinter
+from tkinter import filedialog
 from common import *
 from project.kg import kg
 
@@ -19,12 +21,15 @@ def main():
     crawler.quicky_set_log_path(config)
     # 设置代理
     crawler.quickly_set_proxy(config)
+    # GUI窗口
+    gui = tkinter.Tk()
+    gui.withdraw()
 
     while True:
-        audio_url = input(crawler.get_time() + " 请输入全名K歌歌曲地址：").lower()
+        audio_url = input(crawler.get_time() + " 请输入全民K歌歌曲地址：")
         audio_id = None
         # https://node.kg.qq.com/play?s=JLm8h1J64lEyXJ6n&g_f=personal
-        if audio_url.find("//node.kg.qq.com/play") > 0:
+        if audio_url.lower().find("//node.kg.qq.com/play") > 0:
             query_string_list = audio_url.split("?")[-1].split("&")
             for query_string in query_string_list:
                 if query_string.find("=") == -1:
@@ -42,9 +47,17 @@ def main():
         except crawler.CrawlerException as e:
             log.error("解析歌曲下载地址失败，原因：%s" % e.message)
             tool.process_exit()
-        # 开始下载
+        # 选择下载目录
         file_type = kg.get_file_type(audio_response["audio_url"])
-        file_path = os.path.abspath(os.path.join(DOWNLOAD_FILE_PATH, "%s - %s.%s" % (audio_id, path.filter_text(audio_response["audio_title"]), file_type)))
+        options = {
+            "initialdir": DOWNLOAD_FILE_PATH,
+            "initialfile": "%s - %s.%s" % (audio_id, path.filter_text(audio_response["audio_title"]), file_type),
+            "filetypes": [(file_type, "." + file_type)],
+        }
+        file_path = tkinter.filedialog.asksaveasfilename(**options)
+        if not file_path:
+            continue
+        # 开始下载
         log.step("\n歌曲标题：%s\n歌曲地址：%s\n下载路径：%s" % (audio_response["audio_title"], audio_response["audio_url"], file_path))
         save_file_return = net.save_net_file(audio_response["audio_url"], file_path, head_check=True)
         if save_file_return["status"] == 1:
