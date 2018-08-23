4ef7fc2e29c7967fee2df2f59ed17f84c3bdebe1
hikaru <hikaru870806@hotmail.com>
2018-08-23 11:51:10 +0800

    外链图片处理 & 新增加表情地址过滤 & 一些直接二进制数据的图片 或者 file:// 协议的图片过滤

--- project/ameblo/ameblo.py
+++ project/ameblo/ameblo.py
@@ -11,6 +11,7 @@ import re
 import threading
 import time
 import traceback
+import urllib.parse
 from PIL import Image
 from pyquery import PyQuery as pq
 from common import *
@@ -123,11 +124,19 @@ def get_blog_page(account_name, blog_id):
         if image_selector.has_class("accessLog"):
             continue
         image_url = image_selector.attr("src")
+        # 用户上传的图片
         if image_url.find("//stat.ameba.jp/user_images/") > 0:
+            result["image_url_list"].append(image_url)
+        # 外部图片
+        elif image_url.find("//img-proxy.blog-video.jp/images?url=") > 0:
+            image_url = urllib.parse.unquote(image_url.split("//img-proxy.blog-video.jp/images?url=")[-1])
             result["image_url_list"].append(image_url)
         # 表情
         elif image_url.find("//emoji.ameba.jp/img/") > 0 or image_url.find("//stat.ameba.jp/blog/ucs/img/") > 0 \
-            or image_url.find("//stat100.ameba.jp/blog/ucs/img/") > 0 or image_url.find("//stat.ameba.jp/common_style/") > 0:
+            or image_url.find("//stat100.ameba.jp/blog/ucs/img/") > 0 or image_url.find("//stat.ameba.jp/common_style/") > 0 \
+            or image_url.find("//stat.ameba.jp/mb/") > 0:
+            pass
+        elif image_url.find("data:image/gif;base64,") == 0 or image_url.find("file://") == 0:
             pass
         else:
             log.notice("未知图片地址：%s (%s)" % (image_url, blog_url))
