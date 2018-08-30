150b0f3ac7b675a4937a2eee1e51756b50d6f436
hikaru <hikaru870806@hotmail.com>
2018-08-30 15:16:15 +0800

    import 路径修改

--- common/__init__.py
+++ common/__init__.py
@@ -1,9 +1,6 @@
-from common import crawler
-from common import file
-from common import log
-from common import net
-from common import output
-from common import path
-from common import tool
+try:
+    from . import crawler, file, log, net, output, path, tool
+except ImportError:
+    from common import crawler, file, log, net, output, path, tool
 
 __all__ = ["crawler", "file", "log", "net", "output", "path", "tool"]
