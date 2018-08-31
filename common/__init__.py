b7d334e314a22fc8873e78d2cf6f8dd6cc47fe5f
hikaru <hikaru870806@hotmail.com>
2018-08-31 12:07:24 +0800

    __init__ update

--- common/__init__.py
+++ common/__init__.py
@@ -1,6 +1,1 @@
-try:
-    from . import crawler, file, log, net, output, path, tool
-except ImportError:
-    from common import crawler, file, log, net, output, path, tool
-
 __all__ = ["crawler", "file", "log", "net", "output", "path", "tool"]
