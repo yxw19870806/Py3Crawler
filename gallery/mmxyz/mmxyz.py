8b100279881a60e9eb9d613e53301541e17de987
hikaru <hikaru870806@hotmail.com>
2018-09-04 12:05:45 +0800

    对单个图集进行多线程下载

--- gallery/mmxyz/mmxyz.py
+++ gallery/mmxyz/mmxyz.py
@@ -159,28 +159,41 @@ class MMXYZ(crawler.Crawler):
                     log.error("图集%s《%s》 %s解析失败，原因：%s" % (album_info["album_id"], album_info["album_title"], album_info["album_url"], e.message))
                     raise
 
-                photo_index = 1
                 # 过滤标题中不支持的字符
                 album_title = path.filter_text(album_info["album_title"])
                 if album_title:
                     album_path = os.path.join(self.photo_download_path, "%06d %s" % (album_info["album_id"], album_title))
                 else:
                     album_path = os.path.join(self.photo_download_path, "%06d" % album_info["album_id"])
+
                 temp_path = album_path
+                photo_index = 1
+                thread_list = []
                 for photo_url in album_response["photo_url_list"]:
                     if not self.is_running():
                         tool.process_exit(0)
-                    log.step("图集%s《%s》 开始下载第%s张图片 %s" % (album_info["album_id"], album_info["album_title"], photo_index, photo_url))
-
                     photo_url_split = urllib.parse.urlsplit(photo_url)
                     photo_url = photo_url_split[0] + "://" + photo_url_split[1] + urllib.parse.quote(photo_url_split[2])
+                    log.step("图集%s《%s》 开始下载第%s张图片 %s" % (album_info["album_id"], album_info["album_title"], photo_index, photo_url))
+
+                    # 开始下载
                     file_path = os.path.join(album_path, "%03d.%s" % (photo_index, net.get_file_type(photo_url)))
-                    save_file_return = net.save_net_file(photo_url, file_path)
-                    if save_file_return["status"] == 1:
-                        log.step("图集%s《%s》 第%s张图片下载成功" % (album_info["album_id"], album_info["album_title"], photo_index))
-                    else:
-                        log.error("图集%s《%s》 %s 第%s张图片 %s 下载失败，原因：%s" % (album_info["album_id"], album_info["album_title"], album_info["album_url"], photo_index, photo_url, crawler.download_failre(save_file_return["code"])))
+                    thread = Download(self, file_path, photo_url, photo_index)
+                    thread.start()
+                    thread_list.append(thread)
                     photo_index += 1
+
+                # 等待所有线程下载完毕
+                for thread in thread_list:
+                    thread.join()
+                    save_file_return = thread.get_result()
+                    if save_file_return["status"] != 1:
+                        log.error("图集%s《%s》 %s 第%s张图片 %s 下载失败，原因：%s" % (album_id, album_info["album_title"], album_info["album_url"], thread.photo_index, thread.photo_url, crawler.download_failre(save_file_return["code"])))
+                if self.is_running():
+                    log.step("图集%s《%s》 %s 全部图片下载完毕" % (album_id, album_info["album_title"], album_info["album_url"]))
+                else:
+                    tool.process_exit(0)
+
                 # 图集内图片全部下载完毕
                 temp_path = ""  # 临时目录设置清除
                 self.total_photo_count += photo_index - 1  # 计数累加
@@ -200,6 +213,22 @@ class MMXYZ(crawler.Crawler):
         # 重新保存存档文件
         file.write_file(str(album_id), self.save_data_path, file.WRITE_FILE_TYPE_REPLACE)
         log.step("全部下载完毕，耗时%s秒，共计图片%s张" % (self.get_run_time(), self.total_photo_count))
+
+
+class Download(crawler.DownloadThread):
+    def __init__(self, main_thread, file_path, photo_url, photo_index):
+        crawler.DownloadThread.__init__(self, [], main_thread)
+        self.file_path = file_path
+        self.photo_url = photo_url
+        self.photo_index = photo_index
+        self.result = None
+
+    def run(self):
+        self.result = net.save_net_file(self.photo_url, self.file_path)
+        self.notify_main_thread()
+
+    def get_result(self):
+        return self.result
 
 
 if __name__ == "__main__":
