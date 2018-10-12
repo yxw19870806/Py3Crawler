09a175aa7daf363818017613609731ae49059e02
hikaru <hikaru870806@hotmail.com>
2018-10-13 00:25:52 +0800

    依靠初始化类来获取一些属性&设置

--- project/fiveSing/download.py
+++ project/fiveSing/download.py
@@ -6,7 +6,6 @@ http://5sing.kugou.com/
 email: hikaru870806@hotmail.com
 如有问题或建议请联系
 """
-import os
 import tkinter
 from tkinter import filedialog
 from common import *
@@ -14,9 +13,8 @@ from project.fiveSing import fiveSing
 
 
 def main():
-    config = crawler._get_config()
-    # 设置代理
-    crawler.quickly_set_proxy(config)
+    # 初始化
+    fiveSing_obj = fiveSing.FiveSing()
     # GUI窗口
     gui = tkinter.Tk()
     gui.withdraw()
@@ -45,7 +43,7 @@ def main():
         # 选择下载目录
         file_type = net.get_file_type(audio_response["audio_url"])
         options = {
-            "initialdir": os.path.join(os.path.dirname(__file__), "audio"),
+            "initialdir": fiveSing_obj.audio_download_path,
             "initialfile": "%08d - %s.%s" % (int(audio_id), path.filter_text(audio_response["audio_title"]), file_type),
             "filetypes": [(file_type, "." + file_type)],
             "parent": gui,
