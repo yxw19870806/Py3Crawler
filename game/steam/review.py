420ec429ef77fe288b9ff41fbb5a58cfe3af4437
hikaru <hikaru870806@hotmail.com>
2019-01-12 10:50:20 +0800

    update

--- game/steam/review.py
+++ game/steam/review.py
@@ -15,15 +15,14 @@ from game.steam import steamCommon
 # print_type  1 只要本体
 # print_type  2 只要DLC
 # print_type  3 只要本体已评测的DLC
-def print_list(steam_class, print_type=0):
-    review_data = steam_class.load_cache_apps_info()
-    for game_id in review_data["can_review_lists"]:
+def print_list(apps_cache_data, print_type=0):
+    for game_id in apps_cache_data["can_review_lists"]:
         # 是DLC
-        if game_id in review_data["dlc_in_game"]:
+        if game_id in apps_cache_data["dlc_in_game"]:
             if print_type == 1:
                 continue
             # 本体没有评测过
-            if review_data["dlc_in_game"][game_id] in review_data["can_review_lists"]:
+            if apps_cache_data["dlc_in_game"][game_id] in apps_cache_data["can_review_lists"]:
                 if print_type == 3:
                     continue
         else:
@@ -37,7 +36,7 @@ def main():
     steam_class = steamCommon.Steam(need_login=True)
 
     # 历史记录
-    review_data = steam_class.load_cache_apps_info()
+    apps_cache_data = steam_class.load_cache_apps_info()
     # 获取自己的全部玩过的游戏列表
     try:
         played_game_list = steamCommon.get_account_owned_app_list(steam_class.account_id, True)
@@ -46,7 +45,7 @@ def main():
         raise
     
     for game_id in played_game_list:
-        if game_id in review_data["deleted_list"]:
+        if game_id in apps_cache_data["deleted_list"]:
             continue
 
         # 获取游戏信息
@@ -58,16 +57,16 @@ def main():
 
         # 已删除
         if game_data["deleted"]:
-            review_data["deleted_list"].append(game_id)
+            apps_cache_data["deleted_list"].append(game_id)
         else:
             # 有DLC的话，遍历每个DLC
             for dlc_id in game_data["dlc_list"]:
                 # 已经评测过了，跳过检查
-                if dlc_id in review_data["review_list"]:
+                if dlc_id in apps_cache_data["review_list"]:
                     continue
 
                 # DLC和游戏本体关系字典
-                review_data["dlc_in_game"][dlc_id] = game_id
+                apps_cache_data["dlc_in_game"][dlc_id] = game_id
 
                 # 获取DLC信息
                 try:
@@ -80,39 +79,39 @@ def main():
                     # 已经评测过了
                     if dlc_data["reviewed"]:
                         # 从待评测列表中删除
-                        if dlc_id in review_data["can_review_lists"]:
-                            review_data["can_review_lists"].remove(dlc_id)
+                        if dlc_id in apps_cache_data["can_review_lists"]:
+                            apps_cache_data["can_review_lists"].remove(dlc_id)
                         # 增加已评测记录
-                        if dlc_id not in review_data["review_list"]:
-                            review_data["review_list"].append(dlc_id)
+                        if dlc_id not in apps_cache_data["review_list"]:
+                            apps_cache_data["review_list"].append(dlc_id)
                     # 新的可以评测游戏
                     else:
-                        if dlc_id not in review_data["can_review_lists"]:
-                            review_data["can_review_lists"].append(dlc_id)
+                        if dlc_id not in apps_cache_data["can_review_lists"]:
+                            apps_cache_data["can_review_lists"].append(dlc_id)
 
             # 已经评测过了
             if game_data["reviewed"]:
                 # 从待评测列表中删除
-                if game_id in review_data["can_review_lists"]:
-                    review_data["can_review_lists"].remove(game_id)
+                if game_id in apps_cache_data["can_review_lists"]:
+                    apps_cache_data["can_review_lists"].remove(game_id)
                 # 增加已评测记录
-                if game_id not in review_data["review_list"]:
-                    review_data["review_list"].append(game_id)
+                if game_id not in apps_cache_data["review_list"]:
+                    apps_cache_data["review_list"].append(game_id)
             # 新的可以评测游戏
             else:
-                if game_id not in review_data["can_review_lists"]:
-                    review_data["can_review_lists"].append(game_id)
+                if game_id not in apps_cache_data["can_review_lists"]:
+                    apps_cache_data["can_review_lists"].append(game_id)
 
             # 需要了解
             if game_data["learning"]:
-                if game_id not in review_data["learning_list"]:
-                    review_data["learning_list"].append(game_id)
+                if game_id not in apps_cache_data["learning_list"]:
+                    apps_cache_data["learning_list"].append(game_id)
 
         # 增加检测标记
-        steam_class.save_cache_apps_info(review_data)
+        steam_class.save_cache_apps_info(apps_cache_data)
 
     # 输出
-    print_list(steam_class)
+    print_list(apps_cache_data)
 
 
 if __name__ == "__main__":
