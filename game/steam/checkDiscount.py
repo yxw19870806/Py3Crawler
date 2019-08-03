eb360a70cecd1ac76cd211018eb28a788d6eaac9
hikaru <hikaru870806@hotmail.com>
2019-08-03 22:28:45 +0800

    退出改为跳过

--- game/steam/checkDiscount.py
+++ game/steam/checkDiscount.py
@@ -50,7 +50,7 @@ def main():
             game_data = steamCommon.get_game_store_index(game_id)
         except crawler.CrawlerException as e:
             output.print_msg("游戏%s解析失败，原因：%s" % (game_id, e.message))
-            raise
+            continue
 
         if len(game_data["dlc_list"]) > 0:
             output.print_msg("游戏%s全部DLC: %s" % (game_id, game_data["dlc_list"]))
