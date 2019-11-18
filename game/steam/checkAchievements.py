8803e25aaf22681c8c8bc35e9b68e6f055540f41
hikaru <hikaru870806@hotmail.com>
2019-11-18 11:44:29 +0800

    把方法放入到类中

--- game/steam/checkAchievements.py
+++ game/steam/checkAchievements.py
@@ -9,11 +9,13 @@ from common import output
 from game.steam.lib import astats, steam
 
 
-def main(account_id):
-    owned_game_list = steam.get_account_owned_app_list(account_id)
+def main():
+    steam_class = steam.Steam(need_login=False)
+    owned_game_list = steam.get_account_owned_app_list(steam_class.account_id)
     for game_id in owned_game_list:
         output.print_msg("开始解析游戏 %s" % game_id)
         astats.get_game_invalid_achievements(game_id)
 
+
 if __name__ == "__main__":
-    main(steam.get_account_id_from_file())
+    main()
