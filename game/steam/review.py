0e76ff3263e168887020618f28cc7c579f417464
hikaru <hikaru870806@hotmail.com>
2019-11-19 16:14:22 +0800

    增加参数，是否检测

--- game/steam/review.py
+++ game/steam/review.py
@@ -32,7 +32,7 @@ def print_list(apps_cache_data, game_dlc_list, print_type=0):
         output.print_msg("https://store.steampowered.com/app/%s" % game_id)
 
 
-def main():
+def main(check_game=True):
     # 获取登录状态
     steam_class = steam.Steam(need_login=True)
 
@@ -59,88 +59,89 @@ def main():
         output.print_msg("个人游戏主页解析失败，原因：%s" % e.message)
         raise
 
-    for game_id in played_game_list:
-        if game_id in deleted_app_list:
-            continue
-        if game_id in checked_apps_list:
-            continue
+    if check_game:
+        for game_id in played_game_list:
+            if game_id in deleted_app_list:
+                continue
+            if game_id in checked_apps_list:
+                continue
 
-        output.print_msg("开始解析游戏%s" % game_id)
+            output.print_msg("开始解析游戏%s" % game_id)
 
-        # 获取游戏信息
-        try:
-            game_data = steam.get_game_store_index(game_id)
-        except crawler.CrawlerException as e:
-            output.print_msg("游戏%s解析失败，原因：%s" % (game_id, e.message))
-            continue
+            # 获取游戏信息
+            try:
+                game_data = steam.get_game_store_index(game_id)
+            except crawler.CrawlerException as e:
+                output.print_msg("游戏%s解析失败，原因：%s" % (game_id, e.message))
+                continue
 
-        is_change = False
-        # 已删除
-        if game_data["deleted"]:
-            deleted_app_list.append(game_id)
-            # 保存数据
-            steam_class.save_deleted_app_list(deleted_app_list)
-        else:
-            # 有DLC的话，遍历每个DLC
-            for dlc_id in game_data["dlc_list"]:
-                # 已经评测过了，跳过检查
-                if dlc_id in apps_cache_data["review_list"]:
-                    continue
-
-                # DLC和游戏本体关系字典
-                if dlc_id not in game_dlc_list:
-                    game_dlc_list[dlc_id] = game_id
-                    is_change = True
-
-                # 获取DLC信息
-                try:
-                    dlc_data = steam.get_game_store_index(dlc_id)
-                except crawler.CrawlerException as e:
-                    output.print_msg("游戏%s解析失败，原因：%s" % (dlc_id, e.message))
-                    continue
-
-                if dlc_data["owned"]:
-                    # 已经评测过了
-                    if dlc_data["reviewed"]:
-                        # 从待评测列表中删除
-                        if dlc_id in apps_cache_data["can_review_lists"]:
-                            apps_cache_data["can_review_lists"].remove(dlc_id)
-                        # 增加已评测记录
-                        if dlc_id not in apps_cache_data["review_list"]:
-                            apps_cache_data["review_list"].append(dlc_id)
-                    # 新的可以评测游戏
-                    else:
-                        if dlc_id not in apps_cache_data["can_review_lists"]:
-                            apps_cache_data["can_review_lists"].append(dlc_id)
-
-            # 已经评测过了
-            if game_data["reviewed"]:
-                # 从待评测列表中删除
-                if game_id in apps_cache_data["can_review_lists"]:
-                    apps_cache_data["can_review_lists"].remove(game_id)
-                # 增加已评测记录
-                if game_id not in apps_cache_data["review_list"]:
-                    apps_cache_data["review_list"].append(game_id)
-            # 新的可以评测游戏
+            is_change = False
+            # 已删除
+            if game_data["deleted"]:
+                deleted_app_list.append(game_id)
+                # 保存数据
+                steam_class.save_deleted_app_list(deleted_app_list)
             else:
-                if game_id not in apps_cache_data["can_review_lists"]:
-                    apps_cache_data["can_review_lists"].append(game_id)
+                # 有DLC的话，遍历每个DLC
+                for dlc_id in game_data["dlc_list"]:
+                    # 已经评测过了，跳过检查
+                    if dlc_id in apps_cache_data["review_list"]:
+                        continue
 
-            if is_change:
-                steam_class.save_game_dlc_list(game_dlc_list)
+                    # DLC和游戏本体关系字典
+                    if dlc_id not in game_dlc_list:
+                        game_dlc_list[dlc_id] = game_id
+                        is_change = True
 
-            # 已资料受限制
-            if game_data["restricted"]:
-                if game_id not in restricted_app_list:
-                    restricted_app_list.append(game_id)
-                    # 保存数据
-                    steam_class.save_restricted_app_list(restricted_app_list)
+                    # 获取DLC信息
+                    try:
+                        dlc_data = steam.get_game_store_index(dlc_id)
+                    except crawler.CrawlerException as e:
+                        output.print_msg("游戏%s解析失败，原因：%s" % (dlc_id, e.message))
+                        continue
 
-        # 增加检测标记
-        steam_class.save_cache_apps_info(apps_cache_data)
-        # 保存数据
-        checked_apps_list.append(game_id)
-        file.write_file(",".join(checked_apps_list), checked_apps_file_path, file.WRITE_FILE_TYPE_REPLACE)
+                    if dlc_data["owned"]:
+                        # 已经评测过了
+                        if dlc_data["reviewed"]:
+                            # 从待评测列表中删除
+                            if dlc_id in apps_cache_data["can_review_lists"]:
+                                apps_cache_data["can_review_lists"].remove(dlc_id)
+                            # 增加已评测记录
+                            if dlc_id not in apps_cache_data["review_list"]:
+                                apps_cache_data["review_list"].append(dlc_id)
+                        # 新的可以评测游戏
+                        else:
+                            if dlc_id not in apps_cache_data["can_review_lists"]:
+                                apps_cache_data["can_review_lists"].append(dlc_id)
+
+                # 已经评测过了
+                if game_data["reviewed"]:
+                    # 从待评测列表中删除
+                    if game_id in apps_cache_data["can_review_lists"]:
+                        apps_cache_data["can_review_lists"].remove(game_id)
+                    # 增加已评测记录
+                    if game_id not in apps_cache_data["review_list"]:
+                        apps_cache_data["review_list"].append(game_id)
+                # 新的可以评测游戏
+                else:
+                    if game_id not in apps_cache_data["can_review_lists"]:
+                        apps_cache_data["can_review_lists"].append(game_id)
+
+                if is_change:
+                    steam_class.save_game_dlc_list(game_dlc_list)
+
+                # 已资料受限制
+                if game_data["restricted"]:
+                    if game_id not in restricted_app_list:
+                        restricted_app_list.append(game_id)
+                        # 保存数据
+                        steam_class.save_restricted_app_list(restricted_app_list)
+
+            # 增加检测标记
+            steam_class.save_cache_apps_info(apps_cache_data)
+            # 保存数据
+            checked_apps_list.append(game_id)
+            file.write_file(",".join(checked_apps_list), checked_apps_file_path, file.WRITE_FILE_TYPE_REPLACE)
 
     # 输出
     print_list(apps_cache_data, game_dlc_list)
