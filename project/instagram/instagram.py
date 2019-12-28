e8c7889b1e7efa729bcdfbdd560fbb6fa5b8fac2
hikaru <hikaru870806@hotmail.com>
2019-12-28 23:41:39 +0800

    新的页面结构

--- project/instagram/instagram.py
+++ project/instagram/instagram.py
@@ -173,13 +173,13 @@ def get_media_page(page_id):
     if media_response.status != net.HTTP_RETURN_CODE_SUCCEED:
         raise crawler.CrawlerException(crawler.request_failre(media_response.status))
     media_response_content = media_response.data.decode(errors="ignore")
-    script_json_html = tool.find_sub_string(media_response_content, "window._sharedData = ", ";</script>")
+    script_json_html = tool.find_sub_string(media_response_content, "window.__additionalDataLoaded('/p/%s/'," % page_id, ");</script>")
     if not script_json_html:
         crawler.CrawlerException("页面截取媒体信息失败\n%s" % media_response_content)
     script_json = tool.json_decode(script_json_html)
     if script_json is None:
         raise crawler.CrawlerException("媒体信息加载失败\n%s" % script_json_html)
-    media_info = crawler.get_json_value(script_json, "entry_data", "PostPage", 0, "graphql", "shortcode_media", type_check=dict)
+    media_info = crawler.get_json_value(script_json, "graphql", "shortcode_media", type_check=dict)
     media_type = crawler.get_json_value(media_info, "__typename", type_check=str)
     # 多张图片/视频
     if media_type == "GraphSidecar":
