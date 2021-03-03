90f2d15fcb7bee5d01d027367a7a4b0d06420c89
hikaru <hikaru870806@hotmail.com>
2021-03-03 10:44:09 +0800

    rename

--- common/tools/process/pause.py
+++ common/tools/process/pause.py
@@ -1,13 +1,13 @@
 # -*- coding:UTF-8  -*-
 """
-结束所有爬虫程序
+暂停所有正在运行的爬虫程序
 @author: hikaru
 email: hikaru870806@hotmail.com
 如有问题或建议请联系
 """
 from common import portListenerEvent
-from main import processControl
+from common.tools.process import processControl
 
 
 if __name__ == "__main__":
-    processControl.ProcessControl().send_code(portListenerEvent.PROCESS_STATUS_STOP)
+    processControl.ProcessControl().send_code(portListenerEvent.PROCESS_STATUS_PAUSE)
--- common/tools/process/stop.py
+++ common/tools/process/stop.py
@@ -1,13 +1,13 @@
 # -*- coding:UTF-8  -*-
 """
-继续所有已经暂停的爬虫程序
+结束所有爬虫程序
 @author: hikaru
 email: hikaru870806@hotmail.com
 如有问题或建议请联系
 """
 from common import portListenerEvent
-from main import processControl
+from common.tools.process import processControl
 
 
 if __name__ == "__main__":
-    processControl.ProcessControl().send_code(portListenerEvent.PROCESS_STATUS_RUN)
+    processControl.ProcessControl().send_code(portListenerEvent.PROCESS_STATUS_STOP)
