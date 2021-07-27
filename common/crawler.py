9a1299b025044a7ce86ac9badde13d7683b69ebd
hikaru <hikaru870806@hotmail.com>
2021-07-27 15:08:24 +0800

    revert

--- common/crawler.py
+++ common/crawler.py
@@ -250,12 +250,10 @@ class Crawler(object):
 
         output.print_msg("初始化完成")
 
-    @staticmethod
-    def pause_process():
+    def pause_process(self):
         net.pause_request()
 
-    @staticmethod
-    def resume_process():
+    def resume_process(self):
         net.resume_request()
 
     def stop_process(self):
