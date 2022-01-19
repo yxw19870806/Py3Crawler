59e859c79a4912f4cd66b8526ace05ae87a6f68c
hikaru <hikaru870806@hotmail.com>
2022-01-19 14:20:53 +0800

    当下载失败，检测是否要退出线程

--- project/nogizaka46/nogizaka46_diary.py
+++ project/nogizaka46/nogizaka46_diary.py
@@ -236,15 +236,16 @@ class Download(crawler.DownloadThread):
                     self.step("日志%s的第%s张图片 %s 不符合规则，删除" % (blog_info["blog_id"], photo_index, photo_url))
                     continue
                 else:
-                    self.temp_path_list.append(file_path)
+                    self.temp_path_list.append(file_path)  # 设置临时目录
+                    self.total_photo_count += 1  # 计数累加
                     self.step("日志%s的第%s张图片下载成功" % (blog_info["blog_id"], photo_index))
             else:
                 self.error("日志%s的第%s张图片 %s 下载失败，原因：%s" % (blog_info["blog_id"], photo_index, photo_url, crawler.download_failre(save_file_return["code"])))
+                self.check_thread_exit_after_download_failure()
             photo_index += 1
 
         # 日志内图片全部下载完毕
         self.temp_path_list = []  # 临时目录设置清除
-        self.total_photo_count += photo_index - 1  # 计数累加
         self.account_info[1] = str(blog_info["blog_id"])  # 设置存档记录
 
     def run(self):
