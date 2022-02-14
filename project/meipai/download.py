93be51618f6120b7d329817135d628cd97a869c3
hikaru <hikaru870806@hotmail.com>
2022-02-14 16:22:16 +0800

    调整为类

--- project/meipai/download.py
+++ project/meipai/download.py
@@ -12,43 +12,60 @@ from common import *
 from project.meipai import meipai
 
 
-def main():
-    # 初始化
-    meipai_class = meipai.MeiPai(extra_sys_config={crawler.SYS_NOT_CHECK_SAVE_DATA: True})
-    # GUI窗口
-    gui = tkinter.Tk()
-    gui.withdraw()
+class MeiPaiDownload(meipai.MeiPai):
+    def __init__(self, **kwargs):
+        extra_sys_config = {
+            crawler.SYS_NOT_CHECK_SAVE_DATA: True
+        }
+        meipai.MeiPai.__init__(self, extra_sys_config=extra_sys_config, **kwargs)
 
-    while True:
-        video_url = input(crawler.get_time() + " 请输入美拍视频地址：").lower()
+        # GUI窗口
+        self.gui = tkinter.Tk()
+        self.gui.withdraw()
+
+    @staticmethod
+    def get_video_id_from_console():
+        video_url = input("请输入美拍视频地址：").lower()
         video_id = None
         # http://www.meipai.com/media/209045867
         if video_url.find("//www.meipai.com/media/") > 0:
             video_id = video_url.split("/")[-1].split("?")[0]
         elif tool.is_integer(video_url):
             video_id = video_url
+        return video_id
+
+    def main(self):
+        while True:
+            self.download()
+
+    def download(self):
+        # 输入需要解析的视频
+        video_id = self.get_video_id_from_console()
         if not tool.is_integer(video_id):
             log.step("错误的视频地址，正确的地址格式如：http://www.meipai.com/media/209045867")
-            continue
-        # 访问视频播放页
+            return
+
+        # 获取下载地址
         try:
             video_response = meipai.get_video_play_page(video_id)
         except crawler.CrawlerException as e:
             log.error(e.http_error("视频"))
-            break
+            return
         if video_response["is_delete"]:
             log.step("视频不存在，跳过")
-            continue
+            return
+
         # 选择下载目录
         options = {
-            "initialdir": meipai_class.video_download_path,
+            "initialdir": self.video_download_path,
             "initialfile": "%010d.mp4" % int(video_id),
             "filetypes": [("mp4", ".mp4")],
-            "parent": gui,
+            "parent": self.gui,
         }
         file_path = tkinter.filedialog.asksaveasfilename(**options)
         if not file_path:
-            continue
+            return
+
         # 开始下载
         log.step(f"\n视频地址：{video_response['video_url']}\n下载路径：{file_path}")
         save_file_return = net.download(video_response["video_url"], file_path, head_check=True)
@@ -59,4 +76,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    MeiPaiDownload().main()
