058d8119d43c64a12c64ab771bb0858d0b12604f
hikaru <hikaru870806@hotmail.com>
2022-04-20 23:06:58 +0800

    新的页面结构

--- game/steam/lib/madjoki.py
+++ game/steam/lib/madjoki.py
@@ -14,54 +14,43 @@ COOKIE_INFO = {}
 
 
 def get_banned_game_list():
-    page_count = max_page_count = 1
     result = []
-    while page_count <= max_page_count:
+    is_over = False
+    page_count = 1
+    while not is_over:
         output.print_msg(f"开始解析第{page_count}页删除游戏")
-        index_url = "https://steam.madjoki.com/apps/banned"
+        api_url = "https://ren.madjoki.com/SteamApps/Query"
         query_data = {
-            "type": "1",
             "page": page_count,
-            "desc": "0"
+            "sort": "BannedTime",
+            "owned": "0",
+            "apptype": "1",
+            "list": "banned",
         }
-        index_response = net.request(index_url, method="GET", fields=query_data, is_random_ip=False)
-        if index_response.status != net.HTTP_RETURN_CODE_SUCCEED:
-            raise crawler.CrawlerException(f"第{page_count}页，{crawler.request_failre(index_response.status)}")
-        index_response_content = index_response.data.decode(errors="ignore")
+        api_response = net.request(api_url, method="GET", fields=query_data, is_random_ip=False, json_decode=True)
+        if api_response.status != net.HTTP_RETURN_CODE_SUCCEED:
+            raise crawler.CrawlerException(f"第{page_count}页，{crawler.request_failre(api_response.status)}")
         # 获取游戏名字
-        game_info_selector_list = pq(index_response_content).find("table.table-data tr")
-        for game_index in range(1, game_info_selector_list.length - 1):
+        for game_info in crawler.get_json_value(api_response.json_data, "Items", type_check=list):
             result_game_info = {
                 "game_id": None,  # 游戏ID
                 "game_name": "",  # 游戏名字
                 "game_banned_time": None,  # 游戏下线时间
             }
-            game_info_selector = game_info_selector_list.eq(game_index)
             # 获取游戏名字
-            game_name = game_info_selector.find("td").eq(2).find("a:first").text()
+            game_name = crawler.get_json_value(game_info, "Name", type_check=str)
             if not game_name:
-                raise crawler.CrawlerException("游戏信息截取游戏名字失败\n" + game_info_selector.html())
+                raise crawler.CrawlerException("游戏信息截取游戏名字失败\n" + game_info)
             result_game_info["game_name"] = game_name.strip()
             # 获取游戏ID
-            game_url = game_info_selector.find("td").eq(2).find("a:first").attr("href")
-            game_id = game_url.split("/")[-1]
-            if tool.is_integer(game_id):
-                result_game_info["game_id"] = int(game_id)
+            game_id = crawler.get_json_value(game_info, "ID", type_check=int)
+            result_game_info["game_id"] = game_id
             # 获取游戏下线时间
-            game_banned_time_text = game_info_selector.find("td").eq(3).find("time").attr("datetime")
-            game_banned_time_zone = game_banned_time_text[-6:]
-            game_banned_time_text = game_banned_time_text[:-6]
-            game_banned_time = int(time.mktime(time.strptime(game_banned_time_text.split("+")[0], "%Y-%m-%dT%H:%M:%S")))
-            if game_banned_time_zone[0] == "+":
-                game_banned_time -= int(game_banned_time_zone[1:].split(":")[0]) * 60 * 60
-            elif game_banned_time_zone[0] == "-":
-                game_banned_time += int(game_banned_time_zone[1:].split(":")[0]) * 60 * 60
+            game_banned_time_text = crawler.get_json_value(game_info, "BannedTime", type_check=str)
+            game_banned_time = int(time.mktime(time.strptime(game_banned_time_text, "%Y-%m-%dT%H:%M:%S+00:00")))
             result_game_info["game_banned_time"] = game_banned_time
             result.append(result_game_info)
         # 获取总页数
-        max_page_count = pq(index_response_content).find("nav[aria-label='Pages']:first li.page-item").eq(-2).text()
-        if not tool.is_integer(max_page_count):
-            raise crawler.CrawlerException("页面截取分页信息失败\n" + index_response_content.html())
-        max_page_count = int(max_page_count)
+        is_over = len(result) >= crawler.get_json_value(api_response.json_data, "TotalCount", type_check=int)
         page_count += 1
     return result
