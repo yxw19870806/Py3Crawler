df54a29103677bb08ea43b22c250c9ebbbf8e375
hikaru <hikaru870806@hotmail.com>
2022-07-13 15:50:42 +0800

    获取成员列表

--- project/nogizaka46/get_member_list.py
+++ project/nogizaka46/get_member_list.py
@@ -8,22 +8,29 @@ email: hikaru870806@hotmail.com
 """
 import re
 from common import *
+from pyquery import PyQuery as pq
 from project.nogizaka46 import nogizaka46_diary
 
 
 # 从页面获取全部成员账号
 def get_account_from_index():
-    index_url = "https://blog.nogizaka46.com/"
+    index_url = "https://www.nogizaka46.com/s/n46/diary/MEMBER"
     index_response = net.request(index_url, method="GET")
-    account_list = {}
     if index_response.status != net.HTTP_RETURN_CODE_SUCCEED:
         raise crawler.CrawlerException(crawler.request_failre(index_response.status))
     index_response_content = index_response.data.decode(errors="ignore")
-    member_list_find = re.findall('<div class="unit"><a href="./([^"]*)"><img src="[^>]*alt="([^"]*)" />', index_response_content)
-    if len(member_list_find) == 0:
+    member_selector_list = pq(index_response_content).find("div.ba--ml__list .ba--ml__one__a")
+    if member_selector_list.length == 0:
         raise crawler.CrawlerException("页面截取成员类别失败\n" + index_response_content)
-    for member_info in member_list_find:
-        account_list[member_info[0]] = member_info[1].replace(" ", "")
+    account_list = {}
+    for member_index in range(0, member_selector_list.length):
+        member_selector = member_selector_list.eq(member_index)
+        # 成员id
+        member_index_url_path = member_selector.attr("href")
+        member_id = tool.find_sub_string(member_index_url_path, "&ct=")
+        # 成员名字
+        member_name = member_selector.find(".ba--ml__one__name").text().replace(" ", "")
+        account_list[member_id] = member_name
     return account_list
 
 
@@ -36,7 +43,7 @@ def main():
     if len(account_list_from_api) > 0:
         for account_id in account_list_from_api:
             if account_id not in nogizaka46Diary_class.save_data:
-                nogizaka46Diary_class.save_data[account_id] = [account_id, "", "0", account_list_from_api[account_id]]
+                nogizaka46Diary_class.save_data[account_id] = [account_id, "0", account_list_from_api[account_id]]
         temp_list = [nogizaka46Diary_class.save_data[key] for key in sorted(nogizaka46Diary_class.save_data.keys())]
         file.write_file(tool.list_to_string(temp_list), nogizaka46Diary_class.save_data_path, file.WRITE_FILE_TYPE_REPLACE)
 
