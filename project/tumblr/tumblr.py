44b73ff5795e5c43f218460494f7b1c188436b6b
hikaru <hikaru870806@hotmail.com>
2023-01-30 17:22:22 +0800

    增加失败回调方法

--- project/tumblr/tumblr.py
+++ project/tumblr/tumblr.py
@@ -569,31 +569,11 @@ class CrawlerThread(crawler.CrawlerThread):
                 self.error("%s 存在第三方视频，跳过" % post_description)
                 break
 
-            video_description = "日志%s(%s)视频" % (post_info["post_id"], post_info["post_url"])
-            self.step("开始下载 %s %s" % (video_description, video_url))
-
             video_path = os.path.join(self.main_thread.video_download_path, self.index_key, "%012d.mp4" % post_info["post_id"])
-            download_return = net.Download(video_url, video_path, auto_multipart_download=True)
-            if download_return.status == net.Download.DOWNLOAD_SUCCEED:
-                # 设置临时目录
-                self.temp_path_list.append(video_path)
-                self.step("%s 下载成功" % video_description)
-            else:
-                if download_return.code == 403 and video_url.find("_r1_720") != -1:
-                    video_url = video_url.replace("_r1_720", "_r1")
-                    download_return = net.Download(video_url, video_path, auto_multipart_download=True)
-                    if download_return.status == net.Download.DOWNLOAD_SUCCEED:
-                        self.temp_path_list.append(video_path)  # 设置临时目录
-                        self.total_video_count += 1  # 计数累加
-                        self.step("%s 下载成功" % video_description)
-                        break
-                error_message = "%s %s 下载失败，原因：%s" % (video_description, video_url, crawler.download_failre(download_return.code))
-                # 403、404错误作为step log输出
-                if IS_STEP_ERROR_403_AND_404 and download_return.code in [403, 404]:
-                    self.step(error_message)
-                else:
-                    self.error(error_message)
-                    self.check_download_failure_exit()
+            video_description = "日志%s(%s)视频" % (post_info["post_id"], post_info["post_url"])
+            if self.download(video_url, video_path, video_description, failure_callback=self.download_failure_callback, auto_multipart_download=True):
+                self.temp_path_list.append(video_path)  # 设置临时目录
+                self.total_video_count += 1  # 计数累加
             break
 
         # 图片下载
@@ -628,6 +608,18 @@ class CrawlerThread(crawler.CrawlerThread):
         # 日志内图片和视频全部下载完毕
         self.temp_path_list = []  # 临时目录设置清除
         self.single_save_data[1] = str(post_info["post_id"])  # 设置存档记录
+
+    def download_failure_callback(self, video_url, video_path, video_description, download_return: net.Download):
+        if download_return.code == 403 and video_url.find("_r1_720") != -1:
+            video_url = video_url.replace("_r1_720", "_r1")
+            download_return = net.Download(video_url, video_path, auto_multipart_download=True)
+            if download_return.status == net.Download.DOWNLOAD_SUCCEED:
+                self.step("%s 下载成功" % video_description)
+                return False
+        if IS_STEP_ERROR_403_AND_404 and download_return.code in [403, 404]:
+            self.step("%s %s 下载失败，原因：%s" % (video_description, video_url, download_failre(download_return.code)))
+            return False
+        return True
 
     def _run(self):
         try:
