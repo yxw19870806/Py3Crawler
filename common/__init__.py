b8f693fb7fd457ac8a4ab61ed4851b3c91a30464
hikaru <hikaru870806@hotmail.com>
2023-03-10 22:29:49 +0800

    调整路径常量

--- common/__init__.py
+++ common/__init__.py
@@ -7,6 +7,15 @@ __all__ = [
     "output",
     "path",
     "tool",
+    "PROJECT_ROOT_PATH",
+    "PROJECT_CONFIG_PATH",
 ]
 
+import os
+
 from common.logger import logger as log
+
+# 项目根目录
+PROJECT_ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+# 全局config.ini路径
+PROJECT_CONFIG_PATH = os.path.abspath(os.path.join(PROJECT_ROOT_PATH, "common", "config.ini"))
