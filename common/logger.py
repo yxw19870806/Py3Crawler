ffc730e929479c12de2187937a38ed047dc76cfd
hikaru <hikaru870806@hotmail.com>
2023-03-31 15:17:25 +0800

    type hint & merge function

--- common/logger.py
+++ common/logger.py
@@ -8,10 +8,10 @@ email: hikaru870806@hotmail.com
 import os
 import logging
 from logging import LogRecord
-from typing import Final
+from typing import Any, Final
 from common import color_format, file, tool
 
-DEFAULT_LOG_CONFIG = {
+DEFAULT_LOG_CONFIG: Final[dict[str, Any]] = {
     "IS_CONSOLE_DEBUG": False,
     "IS_CONSOLE_INFO": True,
     "IS_CONSOLE_WARNING": True,
@@ -25,15 +25,18 @@ DEFAULT_LOG_CONFIG = {
     "LOG_WARNING_PATH": "../log/warningLog.txt",
     "LOG_ERROR_PATH": "../log/errorLog.txt",
 }
-LOG_CONFIG: Final = tool.json_decode(file.read_file(os.path.join(os.path.dirname(__file__), "log_config.json")), DEFAULT_LOG_CONFIG)
-LOG_DEBUG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), LOG_CONFIG["LOG_DEBUG_PATH"]))
-LOG_INFO_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), LOG_CONFIG["LOG_INFO_PATH"]))
-LOG_WARNING_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), LOG_CONFIG["LOG_WARNING_PATH"]))
-LOG_ERROR_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), LOG_CONFIG["LOG_ERROR_PATH"]))
+LOG_CONFIG: Final[dict[str, Any]] = tool.json_decode(file.read_file(os.path.join(os.path.dirname(__file__), "log_config.json")), DEFAULT_LOG_CONFIG)
+for key in DEFAULT_LOG_CONFIG:
+    if key not in LOG_CONFIG:
+        LOG_CONFIG[key] = DEFAULT_LOG_CONFIG
+LOG_DEBUG_PATH: str = os.path.abspath(os.path.join(os.path.dirname(__file__), LOG_CONFIG["LOG_DEBUG_PATH"]))
+LOG_INFO_PATH: str = os.path.abspath(os.path.join(os.path.dirname(__file__), LOG_CONFIG["LOG_INFO_PATH"]))
+LOG_WARNING_PATH: str = os.path.abspath(os.path.join(os.path.dirname(__file__), LOG_CONFIG["LOG_WARNING_PATH"]))
+LOG_ERROR_PATH: str = os.path.abspath(os.path.join(os.path.dirname(__file__), LOG_CONFIG["LOG_ERROR_PATH"]))
 
 
 class ColorFormatter(logging.Formatter):
-    log_colors = {
+    log_colors: dict[int, color_format.ColorFormat] = {
         logging.CRITICAL: color_format.ColorFormat(foreground_color=color_format.ForegroundColor.LIGHT_MAGENTA),
         logging.ERROR: color_format.ColorFormat(foreground_color=color_format.ForegroundColor.RED),
         logging.WARNING: color_format.ColorFormat(foreground_color=color_format.ForegroundColor.CYAN),
@@ -53,7 +56,7 @@ class ColorFormatter(logging.Formatter):
 
 
 class FilterConsole(logging.Filter):
-    def filter(self, record: LogRecord):
+    def filter(self, record: LogRecord) -> bool:
         if record.levelno == logging.DEBUG:
             return DEFAULT_LOG_CONFIG["IS_CONSOLE_DEBUG"]
         elif record.levelno == logging.INFO:
@@ -66,24 +69,13 @@ class FilterConsole(logging.Filter):
             return False
 
 
-class FilterDebug(logging.Filter):
-    def filter(self, record: LogRecord):
-        return record.levelno == logging.DEBUG
+class FilterFunc(logging.Filter):
+    def __init__(self, log_level):
+        self.log_level = log_level
+        super().__init__()
 
-
-class FilterInfo(logging.Filter):
-    def filter(self, record: LogRecord):
-        return record.levelno == logging.INFO
-
-
-class FilterWarning(logging.Filter):
-    def filter(self, record: LogRecord):
-        return record.levelno == logging.WARNING
-
-
-class FilterError(logging.Filter):
-    def filter(self, record: LogRecord):
-        return record.levelno == logging.ERROR
+    def filter(self, record: LogRecord) -> bool:
+        return record.levelno == self.log_level
 
 
 logger = logging.getLogger("PyCrawler")
@@ -103,26 +95,26 @@ if DEFAULT_LOG_CONFIG["IS_WRITE_DEBUG"]:
     debug_file_handle = logging.FileHandler(LOG_DEBUG_PATH, encoding="UTF-8")
     debug_file_handle.setLevel(logging.DEBUG)
     debug_file_handle.setFormatter(file_formatter)
-    debug_file_handle.addFilter(FilterDebug())
+    debug_file_handle.addFilter(FilterFunc(logging.DEBUG))
     logger.addHandler(debug_file_handle)
 
 if DEFAULT_LOG_CONFIG["IS_WRITE_INFO"]:
     info_file_handle = logging.FileHandler(LOG_INFO_PATH, encoding="UTF-8")
     info_file_handle.setLevel(logging.INFO)
     info_file_handle.setFormatter(file_formatter)
-    info_file_handle.addFilter(FilterInfo())
+    info_file_handle.addFilter(FilterFunc(logging.INFO))
     logger.addHandler(info_file_handle)
 
 if DEFAULT_LOG_CONFIG["IS_WRITE_WARNING"]:
     warning_file_handle = logging.FileHandler(LOG_WARNING_PATH, encoding="UTF-8")
     warning_file_handle.setLevel(logging.WARNING)
     warning_file_handle.setFormatter(file_formatter)
-    warning_file_handle.addFilter(FilterWarning())
+    warning_file_handle.addFilter(FilterFunc(logging.WARNING))
     logger.addHandler(warning_file_handle)
 
 if DEFAULT_LOG_CONFIG["IS_WRITE_ERROR"]:
     error_file_handle = logging.FileHandler(LOG_ERROR_PATH, encoding="UTF-8")
     error_file_handle.setLevel(logging.ERROR)
     error_file_handle.setFormatter(file_formatter)
-    error_file_handle.addFilter(FilterError())
+    error_file_handle.addFilter(FilterFunc(logging.ERROR))
     logger.addHandler(error_file_handle)
