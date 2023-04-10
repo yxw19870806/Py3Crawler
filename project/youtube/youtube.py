5f71b01b9ae52cf22f5a60c69f01db853eaa8753
hikaru <hikaru870806@hotmail.com>
2023-04-10 15:57:20 +0800

    合并 binary_data

--- project/youtube/youtube.py
+++ project/youtube/youtube.py
@@ -109,7 +109,7 @@ def get_one_page_video(account_id, token):
             "x-youtube-client-name": "1",
             "x-youtube-client-version": "2.20221101.00.00",
         }
-        video_pagination_response = net.request(query_url, method="POST", binary_data=tool.json_encode(post_data), header_list=header_list, json_decode=True)
+        video_pagination_response = net.request(query_url, method="POST", fields=tool.json_encode(post_data), header_list=header_list, json_decode=True)
         if video_pagination_response.status != const.ResponseCode.SUCCEED:
             raise crawler.CrawlerException(crawler.request_failre(video_pagination_response.status))
         video_info_list = crawler.get_json_value(video_pagination_response.json_data, "onResponseReceivedActions", 0, "appendContinuationItemsAction", "continuationItems", type_check=list)
