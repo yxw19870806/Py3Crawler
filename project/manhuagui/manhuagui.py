2b42262c4f6878d5f44a85c6a9731cbfe4acd9cc
hikaru <hikaru870806@hotmail.com>
2023-04-14 16:51:48 +0800

    参数名调整

--- project/manhuagui/manhuagui.py
+++ project/manhuagui/manhuagui.py
@@ -181,7 +181,7 @@ class CrawlerThread(crawler.CrawlerThread):
             photo_path = os.path.join(chapter_path, "%03d.%s" % (photo_index, net.get_file_extension(photo_url)))
             photo_description = "漫画%s %s《%s》第%s张图片" % (chapter_info["chapter_id"], chapter_info["group_name"], chapter_info["chapter_name"], photo_index)
             headers = {"Referer": "https://www.manhuagui.com/comic/%s/%s.html" % (self.index_key, chapter_info["chapter_id"])}
-            if self.download(photo_url, photo_path, photo_description, headers=headers, is_auto_proxy=False):
+            if self.download(photo_url, photo_path, photo_description, headers=headers, is_use_proxy=False):
                 self.total_photo_count += 1  # 计数累加
             photo_index += 1
 
